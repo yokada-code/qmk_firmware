42d62c7034a479d621bf1edf4af6fe8ef9219253
yokd2018 <43539603+yokada-code@users.noreply.github.com>
2023-01-02 21:12:26 -0500

    Add keymap for via firmware and json data

---
+++ keyboards/omkbd/ergodash/mini/keymaps/via/config.h
@@ -0,0 +1,12 @@
+
+#define VENDOR_ID      0xFEED    // From Ergodash via firmware
+#define DESCRIPTION    ErgoDash mini
+
+/*
+// Those items are defined by default
+#define PRODUCT_ID     0xFFF1    // not official, temporary product id.
+#define MANUFACTURER   omkdb
+#define PRODUCT        ErgoDash mini
+*/
+
+
